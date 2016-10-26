4ed9e3ac89d28dacde4b491e58cc991e6653a538
Alberto Marín García <alberto.marin@gmail.com>
2016-10-26 22:18:11 +0200

    Primer commit con la estructura básica del proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    azuremobileapps = require("azure-mobile-apps");
+
+var app = express(),
+    mobile = azuremobileapps( { swagger: process.env.NODE_ENV !== 'production' } );
+
+mobile.tables.import("./tables");
+
+mobile.api.import("./api");
+
+app.use(mobile);
+
+app.listen(process.env.PORT || 3000);
