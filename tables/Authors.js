361a4b70e3483177b014c7fe609d6d2f89de5d7b
Alberto Marín García <alberto.marin@gmail.com>
2016-10-28 00:23:33 +0200

    Versionando Notes

---
+++ tables/Authors.js
@@ -0,0 +1,32 @@
+/**
+ * Created by alberto on 19/10/16.
+ */
+
+var azureMobileApps = require('azure-mobile-apps');
+
+
+var table = azureMobileApps.table();
+table.columns = {
+    "name": "string",
+    "surname": "string"
+};
+
+table.dynamicSchema = false;
+
+table.insert(function (context) {
+    context.item.idUsuario = context.user.id;
+    return context.execute();
+});
+
+table.read(function (context) {
+    context.query.where( { idUsuario : context.user.id } );
+    return context.execute();
+});
+
+table.read.access = 'anonymous';
+table.update.access = 'authenticated';
+table.delete.access = 'authenticated';
+table.insert.access = 'authenticated';
+//table.update.access = 'disabled';
+
+module.exports = table;
