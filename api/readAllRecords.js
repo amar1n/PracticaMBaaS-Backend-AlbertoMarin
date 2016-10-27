b543439f13c330aec21b5e4f90d1e970a72be628
Alberto Marín García <alberto.marin@gmail.com>
2016-10-28 01:18:58 +0200

    Versionando Notes

---
+++ api/readAllRecords.js
@@ -0,0 +1,28 @@
+/**
+ * Created by alberto on 20/10/16.
+ */
+
+var api = {
+    get: function (req, res, next) {
+        if (req.param.length < 0) {
+            return next();
+        }
+
+        var context = req.azureMobile;
+        var userId = context.user.id;
+        var query = {
+            sql: "select * from Notes"
+        };
+
+        context.data.execute(query)
+            .then(function (result) {
+                res.json(result);
+            });
+    }
+
+    //put: functi..
+};
+
+api.get.access = 'authenticated';
+
+module.exports = api;
