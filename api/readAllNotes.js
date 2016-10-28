bcd5f4ce80ef4f565d0d4a5d06e49fe83c9820cb
Alberto Marín García <alberto.marin@gmail.com>
2016-10-28 12:46:39 +0200

    Rename del api que lee las notas

---
+++ api/readAllNotes.js
@@ -0,0 +1,30 @@
+/**
+ * Created by alberto on 20/10/16.
+ */
+
+var api = {
+    get: function (req, res, next) {
+        if (req.param.length < 0) {
+            return next();
+        }
+
+        // var userId = context.user.id;
+        // console.log('...................userId: ' + userId);
+
+        var context = req.azureMobile;
+        var query = {
+            sql: "select * from Notas"
+        };
+
+        context.data.execute(query)
+            .then(function (result) {
+                res.json(result);
+            });
+    }
+
+    //put: functi..
+};
+
+api.get.access = 'anonymous';
+
+module.exports = api;
