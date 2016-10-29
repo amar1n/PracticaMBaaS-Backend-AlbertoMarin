d71567e859f153770dd8cdcfc12e54055169832b
Alberto Marín García <alberto.marin@gmail.com>
2016-10-29 20:45:00 +0200

    Api de update de votos

--- api/updatePoints.js
+++ api/updatePoints.js
@@ -3,25 +3,6 @@
  */
 
 var api = {
-    get: function (req, res, next) {
-        if (req.param.length < 0) {
-            return next();
-        }
-
-        // var userId = context.user.id;
-        // console.log('...................userId: ' + userId);
-
-        var context = req.azureMobile;
-        var query = {
-            sql: "select * from Notas where deleted = 0"
-        };
-
-        context.data.execute(query)
-            .then(function (result) {
-                res.json(result);
-            });
-    },
-
     post: function (req, res, next) {
 
         //this is how we access the table
