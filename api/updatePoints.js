2ad8541bf1a4473e141b66341034cb764be6b78e
Alberto Marín García <alberto.marin@gmail.com>
2016-10-29 21:00:24 +0200

    Api de update de votos

--- api/updatePoints.js
+++ api/updatePoints.js
@@ -4,14 +4,18 @@
 
 var api = {
     get: function (req, res, next) {
+        console.log(".....................1");
         if (req.param.length < 0) {
             return next();
         }
+        console.log(".....................2");
 
         var context = req.azureMobile;
+        console.log(".....................3");
         var query = {
             sql: "update Notas set puntos = " + req.query.puntos
         };
+        console.log(".....................4");
 
         context.data.execute(query)
             .then(function (result) {
